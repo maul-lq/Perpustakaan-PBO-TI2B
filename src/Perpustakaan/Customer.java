4fa07bb880f6a045ac38259841e85eba20b15b8e
Rizlrad Fz <rizlradfz@gmail.com>
2025-03-26 09:45:02 +0700

    Pre update crud customer

--- src/Perpustakaan/Customer.java
+++ src/Perpustakaan/Customer.java
@@ -1,16 +1,37 @@
 package Perpustakaan;
 
+import java.util.ArrayList;
+
 public class Customer {
+    private int idCustomer;
+    
     private String userName;
     private String password;
     private String peminjamanBuku;
-
+    private ArrayList<Customer> customers = new ArrayList<>();
+    
     public Customer(String userName, String password, String peminjamanBuku) {
         this.userName = userName;
         this.password = password;
         this.peminjamanBuku = peminjamanBuku;
     }
 
+    public int getIdCustomer() {
+        return this.idCustomer;
+    }
+
+    public void setIdCustomer(int idCustomer) {
+        this.idCustomer = idCustomer;
+    }
+    
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
     public String getUserName() {
         return this.userName;
     }
@@ -27,19 +48,41 @@ public class Customer {
         this.peminjamanBuku = peminjamanBuku;
     }
 
-    public void UpdateCustomer() {
-        Customer customer = new Customer("johndoe", "securepass", "Harry Potter");
-        customer.setUserName("janedoe");
-        customer.setPeminjamanBuku("Lord of the Rings");
-        System.out.println("Updated Customer: " + customer.getUserName());
-        System.out.println("Updated Buku yang dipinjam: " + customer.getPeminjamanBuku());
+    public void updateCustomerById(int idCustomer, String newUserName, String newPassword, String newPeminjamanBuku) {
+        for (Customer customer : this.customers) {
+            if (customer.getIdCustomer() == idCustomer) {
+                customer.setUserName(newUserName);
+                customer.setPassword(newPassword);
+                customer.setPeminjamanBuku(newPeminjamanBuku);
+                System.out.println("Customer data berhasil diupdate");
+                return;
+            }
+        }
+        System.out.println("Customer dengan id " + idCustomer + " tidak ditemukan");
     }
 
-    public void tampilakanCostumer() {
-        System.out.println("Customer: " + this.getUserName());
-        System.out.println("Buku yang dipinjam: " + this.getPeminjamanBuku());
-        System.out.println(" ");
+    public void hapusCustomer(Customer customer) {
+        this.customers.remove(customer);
+        System.out.println("Customer berhasil dihapus");
     }
+
+    private int idCounter = 1;
+    public void tambahCustomer(Customer customer) {
+        customer.setIdCustomer(this.idCounter);
+        this.customers.add(customer);
+        System.out.println("Customer berhasil ditambahkan");
+        this.idCounter++;
+    }
+    
+    public void tampilkanCostumer() {
+        for (Customer customer : this.customers) {
+            System.out.println("Customer: " + customer.getUserName());
+            System.out.println("Buku yang dipinjam: " + customer.getPeminjamanBuku());
+            System.out.println(" ");
+        }
+        System.out.println("Jumlah Customer: " + this.customers.size());
+    }
+
 
     // public boolean login(String x, String y){
     // if (this.userName == x && this.password == y){
